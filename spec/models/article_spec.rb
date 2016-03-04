66dc23cacb4994091d1199cfe9870cbf022459fc
Alexander Zhadnov <masteralred@gmail.com>
2016-03-04 18:16:22 +0300

    Add inside validation blocks for models (more readable code)

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 describe Article do
-  it { should have_many :comments }
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :text }
+
+  describe 'associations' do
+    it { should have_many :comments }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
+  
 end
