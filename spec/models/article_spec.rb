41ecf015a2c6510d5a4a948d33b16b0f5a41c55a
Alexander Zhadnov <masteralred@gmail.com>
2016-03-07 18:55:48 +0300

    Add test for new method of 'Article' model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,5 +10,14 @@ describe Article do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
   end
-  
+
+  describe '#subject' do
+    it 'returns article title' do
+      # Arrange (prepare) object with Factory Girl
+      article = create(:article, title: 'Test article')
+
+      # Assert (checking/test)
+      expect(article.subject).to eq 'Test article'
+    end
+  end
 end
