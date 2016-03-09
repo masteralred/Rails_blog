a3fbfb40c4637d9c2e5678795eb0e95e9c88b504
Alexander Zhadnov <masteralred@gmail.com>
2016-03-09 13:12:35 +0300

    Additional factory for 'Article' model (instance with multi comments)

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,13 @@ FactoryGirl.define do
   factory :article do
     title "Article title"
     text "Article text"
+
+    # create factory named 'article_with_comments'
+    # for article with several comments connected
+    factory :article_with_comments do
+      after :create do |article, evaluator|
+        create_list :comment, 3, article: article
+      end
+    end
   end
 end
