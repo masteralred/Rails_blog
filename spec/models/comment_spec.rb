764841d80c78a3cda0338ef61f32fe5b1163ae95
Alexander Zhadnov <masteralred@gmail.com>
2016-03-09 14:04:31 +0300

    Add test for Comment body length validation

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,6 +8,7 @@ describe Comment do
 
   describe 'validations' do
     it { should validate_presence_of :body }
+    it { should validate_length_of(:body).is_at_least(3).is_at_most(4000).on(:create) }
   end
   
 end
