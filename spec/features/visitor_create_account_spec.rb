dfc8bc4a04ab84866c273355c7086241c8a8ed13
Alexander Zhadnov <masteralred@gmail.com>
2016-03-10 16:14:53 +0300

    Move 'sign_up' code from test file to new created helper for usability

--- spec/features/visitor_create_account_spec.rb
+++ spec/features/visitor_create_account_spec.rb
@@ -2,15 +2,7 @@ require 'rails_helper'
 
 feature 'Account Creation' do
   scenario 'allows guest to create account' do
-    visit new_user_registration_path
-    
-    fill_in :user_email, :with => 'user@example.com'
-    fill_in :user_username, :with => 'exampleusername'
-    fill_in :user_password, :with => '12345678'
-    fill_in :user_password_confirmation, :with => '12345678'
-
-    click_button 'Sign up'
-
+    sign_up
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
 end
---
+++ spec/support/session_helper.rb
@@ -0,0 +1,10 @@
+def sign_up
+  visit new_user_registration_path
+    
+  fill_in :user_email, :with => 'user@example.com'
+  fill_in :user_username, :with => 'exampleusername'
+  fill_in :user_password, :with => '12345678'
+  fill_in :user_password_confirmation, :with => '12345678'
+
+  click_button 'Sign up'
+end
