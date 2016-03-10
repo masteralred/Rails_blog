563a428c7f77340a486ed99c8b316edd22399e1f
Alexander Zhadnov <masteralred@gmail.com>
2016-03-10 11:20:46 +0300

    Add first 'capybara' contact acceptance test

---
+++ spec/features/visitor_create_contact_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+feature 'Contact Creation' do
+  scenario 'allows access to contacts page' do
+    visit '/contacts'
+    expect(page).to have_content 'Contact us'
+  end
+end
