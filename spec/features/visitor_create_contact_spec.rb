22d4123bfae4d06fdc833a4340234a586ecedd0b
Alexander Zhadnov <masteralred@gmail.com>
2016-03-10 12:32:13 +0300

    Update contact acceptance test with form submitting

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -5,4 +5,12 @@ feature 'Contact Creation' do
     visit '/contacts'
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
+
+  scenario 'allows a guest to create contact' do
+    visit '/contacts'
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'message'
+    click_button 'Отправить сообщение'
+    expect(page).to have_content 'спасибо за отзыв!'
+  end
 end
