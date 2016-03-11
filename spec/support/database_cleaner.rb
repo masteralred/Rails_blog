85c1e4dce485524b15cf08b77a0b0ab49fd4db30
Alexander Zhadnov <masteralred@gmail.com>
2016-03-11 11:10:40 +0300

    Small update to Database Cleaner config file

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -2,13 +2,18 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 
 end
