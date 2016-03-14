6e57f280ce1789ddf4603c4db64097f640f22421
Alexander Zhadnov <masteralred@gmail.com>
2016-03-14 10:17:01 +0300

    Add test 'comment creation'

---
+++ spec/features/user_create_comment_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'Comment Creation' do
+  before do
+    sign_up
+    article_create
+  end
+
+  scenario 'allows user to visit article page' do
+    visit article_path(1)
+    expect(page).to have_content 'New article body'
+  end
+
+  scenario 'allows user to create commentary' do
+    visit article_path(1)
+    fill_in :comment_body, :with => 'Some text'
+    click_button 'Добавить'
+    expect(page).to have_content 'Some text'
+  end
+end
